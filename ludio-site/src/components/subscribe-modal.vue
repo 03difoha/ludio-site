473d9409e3afcbfad64c8daac647141875875bf5
Harry Difolco <h@difol.co>
2018-12-31 01:50:53 +0700

    testing sub modal

--- ludio-site/src/components/subscribe-modal.vue
+++ ludio-site/src/components/subscribe-modal.vue
@@ -1,42 +1,24 @@
 <template>
   <div>
-    <b-btn v-b-modal.modalPrevent>Launch demo modal</b-btn>
-    <!-- Main UI -->
-    <div class="mt-3 mb-3">
-      Submitted Names:
-      <ul>
-        <li v-for="n in names">{{n}}</li>
-      </ul>
-    </div>
+    <b-btn v-b-modal.modalPrevent>Launch demo modal</b-btn>    
     <!-- Modal Component -->
     <b-modal id="modalPrevent"
              ref="modal"
              @ok="handleOk"
              :title="title"
              @shown="clearName">
-      <form @submit.stop.prevent="handleSubmit" method="POST">
-        <b-form-input hidden name="u" value="fb66abc77bce32858a9bdf813"></b-form-input>
-        <b-form-input hidden name="id" value="67afc8c3d2"></b-form-input>
-        <b-form-input name="MERGE1" id="MERGE1" size="25" value="" type="text" placeholder="Enter your name" v-model="name"></b-form-input>
-        <b-form-input type="email" autocapitalize="off" autocorrect="off" name="MERGE0" id="MERGE0" size="25" value="" placeholder="Enter your email" v-model="email"></b-form-input>
+      <form @submit.stop.prevent="handleSubmit">
+        <b-form-input type="email" autocapitalize="off" autocorrect="off" name="EMAIL" size="25" value="" placeholder="Enter your email" v-model="email"></b-form-input>
       </form>
-      <!-- <form action="https://xyz.us7.list-manage.com/subscribe/post" method="POST">
-      <input type="hidden" name="u" value="fb66abc77bce32858a9bdf813">
-      <input type="hidden" name="id" value="67afc8c3d2">
-      <input type="text" name="MERGE1" id="MERGE1" size="25" value="">
-      <input type="email" autocapitalize="off" autocorrect="off" name="MERGE0" id="MERGE0" size="25" value="">
-      </form> -->
     </b-modal>
-  </div>
+    </div>   
 </template>
 
 <script>
 export default {
   data () {
     return {
-      name: '',
       email: '',
-      names: [],
       title: 'Like to follow updates with Ludio?'
     }
   },
@@ -45,7 +27,7 @@ export default {
     handleOk (evt) {
       // Prevent modal from closing
       evt.preventDefault()
-      if (!this.name) {
+      if (!this.email) {
         alert('Please enter your email')
       } else {
         this.handleSubmit()
@@ -53,25 +35,10 @@ export default {
     },
     clearName(){},
     handleSubmit () {
-      this.names.push(this.name)
       // POST /someUrl
-  this.$http.post('https://xyz.us7.list-manage.com/subscribe/post', {foo: 'bar'}).then(response => {
-
-    // get status
-    response.status;
-
-    // get status text
-    response.statusText;
-
-    // get 'Expires' header
-    response.headers.get('Expires');
-
-    // get body data
-    this.someData = response.body;
-
-  }, response => {
-    // error callback
-  });
+      console.log(this.email)
+      this.$http.post('https://cors-anywhere.herokuapp.com/https://xyz.us7.list-manage.com/subscribe/post-json?u=fb66abc77bce32858a9bdf813&amp;id=67afc8c3d2&c=?',
+      {body: JSON.stringify({ 'EMAIL': this.email })})
     }
   }
 }
